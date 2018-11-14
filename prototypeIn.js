f5a425846afe8e71b3a0ff3128b0c49bf2fe9cbd
msharaf1 <menhaj0102@gmail.com>
2018-11-14 17:13:47 -0500

    commit

--- prototypeIn.js
+++ prototypeIn.js
@@ -49,3 +49,34 @@ Dog.prototype.numLegs = 4;
 
 // Add your code above this line
 let beagle = new Dog("Snoopy");
+
+
+///Object Oriented Programming: Iterate Over All Properties
+//You have now seen two kinds of properties: own properties and prototype properties. Own properties are defined directly on the object instance itself. And prototype properties are defined on the prototype.
+////freecodecamp
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Add your code below this line 
+for(let property in beagle){
+  if(beagle.hasOwnProperty(property)){
+    ownProps.push(property);
+  }else{
+    prototypeProps.push(property);
+  }
+}
+
+console.log(ownProps);
+console.log(prototypeProps);
+
+
+
