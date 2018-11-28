0de37adeec5fe91bc5ac86c407f1fd54843920aa
msharaf1 <menhaj0102@gmail.com>
2018-11-27 23:54:08 -0500

    commit

--- tipCalc/tipCal.js
+++ tipCalc/tipCal.js
@@ -7,18 +7,31 @@ function calcTip(billAmount){
     };
 
     if(billAmount > 0 && billAmount <=50){
-        tip.push(billAmount * 0.2);
+        tip.push(parseFloat(billAmount * 0.2));
         bill.push((billAmount) + (billAmount * 0.2));
-    } else if(billAmount >50){
-        tip.push(billAmount * 0.15);
+    } else if(billAmount >50 && billAmount <=100){
+        tip.push(parseFloat(billAmount * 0.15));
         bill.push((billAmount) + (billAmount * 0.15));
-    }else if(billAmount > 200){
-        tip.push(billAmount * 0.10);
+    } else if(billAmount >100 && billAmount <=200){
+        tip.push(parseFloat(billAmount * 0.10));
+        bill.push((billAmount) + (billAmount * 0.15));
+    }else if(billAmount > 200 && billAmount <=1000){
+        tip.push(parseFloat(billAmount * 0.10));
         bill.push(billAmount + (billAmount * 0.10));
-    };
+    } else if(billAmount > 1000){
+        tip.push(parseFloat(billAmount * 0.05));
+        bill.push(billAmount + (billAmount * 0.05));
+    }
     console.log(bill);
     if(billAmount!= null ){
         document.getElementById("tPercent").innerHTML = ("Bill Amount is : $ "+ billAmount +", Tip Ampount is $ " +tip + ". Total is: $ " + bill);
     }
+
+    if(billAmount!= null && billAmount !=NaN){
+        document.getElementById("bAmount").innerHTML = (billAmount);
+        document.getElementById("pAmount").innerHTML = (tip);
+        document.getElementById("tAmount").innerHTML = (bill);
+    }
+
 }
 calcTip(billAmount);
