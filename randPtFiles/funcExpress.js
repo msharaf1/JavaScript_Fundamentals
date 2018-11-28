50daaa2a7bd69c961675e392edfc1c779d33b6ea
msharaf1 <menhaj0102@gmail.com>
2018-11-28 16:49:28 -0500

    commit

--- randPtFiles/funcExpress.js
+++ randPtFiles/funcExpress.js
@@ -6,7 +6,7 @@ let ageOfJason = calcAge(1985);
 let agOfTim = calcAge(1990);
 let ageOfMason = calcAge(2000);
 
-console.log("Jason is : "+ ageOfJason +" years old.");
+// console.log("Jason is : "+ ageOfJason +" years old.");
 // console.log("Tim is : "+ agOfTim +" years old.");
 // console.log("Jason is : "+ ageOfMason +" years old.");
 
@@ -21,7 +21,7 @@ function yrsUntilRetirement(year, name){
     }
 }
 
-console.log(yrsUntilRetirement(1980, "Tina"));
+// console.log(yrsUntilRetirement(1980, "Tina"));
 
 //Function Expression
 
@@ -36,10 +36,10 @@ const jobSurvey = function (job, name){
     }
 }
 
-console.log(jobSurvey("teacher", "Kamal"));
+// console.log(jobSurvey("teacher", "Kamal"));
 
 let Khan = jobSurvey("desinger", "Rashid Khan");
-console.log(Khan);
+// console.log(Khan);
 
 const interJob = function(job, name){
     if(job ==="teacher"){
@@ -51,5 +51,28 @@ const interJob = function(job, name){
         name +" "+ "doesn't live in the city.";
     }
 }
-console.log(interJob("teacher", "John"));
-console.log(interJob("soccer", "Kamal"));
+// console.log(interJob("teacher", "John"));
+// console.log(interJob("soccer", "Kamal"));
+
+
+
+
+
+function tipCal(bill){
+    let percentage;
+    if(bill < 50){
+        percentage = 0.2;//20%
+    } else if(bill >= 50 && bill <= 200){
+        percentage = 0.15;//15%
+    } else {
+        percentage = 0.1;
+    }
+    return bill * percentage;
+}//tipCall function bracket
+
+const bill = [124,45, 300];
+const tips = [tipCal(bill[0]), tipCal(bill[1]), tipCal(bill[2])];
+console.log(tips);
+const totalAmount = [bill[0] + tips[0], bill[1]+tips[1]];
+console.log(totalAmount);
+
