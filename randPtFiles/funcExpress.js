3b22f4d33029f7a38e43f648b56edbc9202a0916
msharaf1 <menhaj0102@gmail.com>
2018-11-27 21:55:13 -0500

    commit

---
+++ randPtFiles/funcExpress.js
@@ -0,0 +1,42 @@
+function calcAge(DoB){
+    return 2018 - DoB;
+}
+
+let ageOfJason = calcAge(1985);
+let agOfTim = calcAge(1990);
+let ageOfMason = calcAge(2000);
+
+console.log("Jason is : "+ ageOfJason +" years old.");
+// console.log("Tim is : "+ agOfTim +" years old.");
+// console.log("Jason is : "+ ageOfMason +" years old.");
+
+function yrsUntilRetirement(year, name){
+    let age = calcAge(year);
+    let retirement = 65 - age;
+
+    if(retirement > 0){
+        return (`${name} retires in ${retirement} years.`);
+    } else {
+        return (`${name} is already retired.`);
+    }
+}
+
+console.log(yrsUntilRetirement(1980, "Tina"));
+
+//Function Expression
+
+const jobSurvey = function (job, name){
+    switch (job){
+        case "teacher":
+            return (name +" teaching at elementary school");
+        case "designer":
+            return (name +" is a graphic designer at MDDX.");
+        default:
+            return (name + " is an out of state resident.")
+    }
+}
+
+console.log(jobSurvey("teacher", "Kamal"));
+
+let Khan = jobSurvey("desinger", "Rashid Khan");
+console.log(Khan);
