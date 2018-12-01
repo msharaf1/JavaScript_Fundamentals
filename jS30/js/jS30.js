f2a94a696d67870b9c27a5fea65ed521174d77f3
msharaf1 <menhaj0102@gmail.com>
2018-12-01 14:07:09 -0500

    commit

---
+++ jS30/js/jS30.js
@@ -0,0 +1,56 @@
+// alert("Hello, this is a test")
+// function mainCode(){
+
+
+ function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // console.log(audio);
+    if(!audio){
+        return;  
+    } else{
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add("playing");
+
+    }
+};
+
+function removeTransition(e){
+    // console.log(e);
+    if(e.propertyName !== "transform") return;
+    this.classList.remove("palying");
+}
+
+    const keys = document.querySelectorAll(".key");
+    keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+    window.addEventListener("keydown", playSound);
+
+
+
+// }//maoin funciton
+// mainCode();
+
+// function copiedCode(){
+//     function removeTransition(e) {
+//         if (e.propertyName !== 'transform') return;
+//         e.target.classList.remove('playing');
+//       }
+    
+//       function playSound(e) {
+//         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//         const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//         if (!audio) return;
+    
+//         key.classList.add('playing');
+//         audio.currentTime = 0;
+//         audio.play();
+//       }
+    
+//       const keys = Array.from(document.querySelectorAll('.key'));
+//       keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//       window.addEventListener('keydown', playSound);
+
+//     //
+// }//Function Bracket
+// copiedCode();
---
+++ pGame/js/pGame.js
@@ -0,0 +1,5 @@
+let gScore = 0;
+
+document.querySelector('.btn-hold').addEventListener('click'. function(){
+                                                     gScore = current score;
+                                                     })
