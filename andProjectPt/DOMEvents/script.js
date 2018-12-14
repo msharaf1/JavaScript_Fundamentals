4e57ac76dab1e7fc90a0abfcf94b8fa05790990a
msharaf1 <menhaj0102@gmail.com>
2018-12-13 23:11:47 -0500

    commit

--- andProjectPt/DOMEvents/script.js
+++ andProjectPt/DOMEvents/script.js
@@ -6,10 +6,31 @@
 
 const button = document.getElementById("enter");
 const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
 
 button.addEventListener("click", function(){
-    console.log("test");
-})
+    // console.log("test");
+    if(input.value.length >0){
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+});
+
+input.addEventListener("keypress", function(event){
+    // console.log("test");
+    // event.preventDefault(); don't use it here.
+    if(input.value.length > 0 && event.keyCode===13){
+    // if(input.value.length >0){
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+// }
+});
+
 
 
     // let myList = document.querySelectorAll("li");
