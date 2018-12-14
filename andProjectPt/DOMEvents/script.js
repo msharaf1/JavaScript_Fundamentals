f40efc75e9b328eb1f5f0257e88c66dd1aca7ecc
msharaf1 <menhaj0102@gmail.com>
2018-12-13 22:46:48 -0500

    commit

---
+++ andProjectPt/DOMEvents/script.js
@@ -0,0 +1,18 @@
+// const button = document.getElementsByTagName("button")[0];
+// button.addEventListener("click", function(){
+//     console.log("clicked");
+// })
+
+
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+
+button.addEventListener("click", function(){
+    console.log("test");
+})
+
+
+    // let myList = document.querySelectorAll("li");
+    // for(let i=0; i<myList.length; i++){
+    //     console.log(myList[i]);
+    // }
